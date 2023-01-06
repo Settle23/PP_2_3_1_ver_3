c767d634563a34156ebb193b1b197fa889635f05
Vsevolod <106489473+Settle23@users.noreply.github.com>
2023-01-07 00:46:29 +0300

    Initial commit

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,71 @@
+package web.controller;
+
+import web.model.User;
+import web.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired()
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String getUsers(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "users";
+    }
+
+    @GetMapping("/{id}")
+    public String getUserById(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "user";
+    }
+
+    @GetMapping("/new")
+    public String getUserCreationForm(User user) {
+        return "create";
+    }
+
+    @PostMapping("/new")
+    public String createNewUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "create";
+        } else {
+            userService.addUser(user);
+            return "redirect:/";
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteUserById(@PathVariable("id") long id) {
+        userService.removeUser(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("edit/{id}")
+    public String getUserUpdateForm(@PathVariable("id") long id, Model model) {
+        model.addAttribute(userService.getUserById(id));
+        return "edit";
+    }
+
+    @PatchMapping("/edit")
+    public String updateUserData(@Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "edit";
+        } else {
+            userService.updateUser(user);
+            return "redirect:/";
+        }
+    }
+}
